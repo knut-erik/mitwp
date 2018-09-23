e23a13ecd764ae7602c78c43a4674beaf5e560f4
Knut Erik Hollund <knut.erik@unlike.no>
2018-09-23 22:34:19 +0200

    added some responsiveness to the table

--- mitwp-import.php
+++ mitwp-import.php
@@ -41,90 +41,80 @@ function renderHTML() {
 
     ?>
        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                        <div class="alert alert-success">
+                            <strong><?php _e('HOME URL : ','mitwp'); echo get_home_url(); ?>&nbsp;-&nbsp;<?php _e('USER: ','mitwp'); ?>&nbsp;<?php echo wp_get_current_user()->display_name ?></strong>
+                            <br>
+                            <textarea readonly class="form-control" rows="4" id="log"></textarea>
+                        </div>
+                </div><!-- col -->
+            </div><!-- row -->
 
-        <div class="row">
-           <div class="col-md-12">
-                <div class="alert alert-success">
-                    <strong><?php _e('HOME URL : ','mitwp'); echo get_home_url(); ?>&nbsp;-&nbsp;<?php _e('USER: ','mitwp'); ?>&nbsp;<?php echo wp_get_current_user()->display_name ?></strong>
-                </div>
-            </div>
-        </div>    
-        <div id="home_url" style="display: none;"><?php echo get_home_url() ?></div>
-        <div id="wp_user_id" style="display: none;"><?php echo get_current_user_id() ?></div>
-        <div id="labora_url" style="display: none;"><?php echo constant('LABORA_URL'); ?></div>
-        <div id="labora_url_params" style="display: none;"><?php echo constant('LABORA_URL_PARAMS'); ?></div>
-
-
-       <div class="row">
-           <div class="col-md-12">
-                <div class="form-group">
-                    <label for="comment">LOG:</label>
-                    <textarea readonly class="form-control" rows="5" id="log"></textarea>
-                </div>
-           </div>
-       </div>
+            <div id="home_url" style="display: none;"><?php echo get_home_url() ?></div>
+            <div id="wp_user_id" style="display: none;"><?php echo get_current_user_id() ?></div>
+            <div id="labora_url" style="display: none;"><?php echo constant('LABORA_URL'); ?></div>
+            <div id="labora_url_params" style="display: none;"><?php echo constant('LABORA_URL_PARAMS'); ?></div>
 
        <!-- ROW -->
         <div class="row">
-            <!-- LEFT COLUMN -->
-            <div class="col-md-2">
-                            <div class="dropdown pull-left">
-                                <button id="btn_choose_category" type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown"><?php _e('Choose Group','mitwp'); ?>&nbsp;<span class='glyphicon glyphicon-folder-open'></span>&nbsp;&nbsp;<span class="caret"></span></button>
-                                <ul class="dropdown-menu">
-                                    <li><a href="javascript:getiCalFromUrl('<?php echo constant('GUDSTJENESTE_ICAL_UID'); ?>'
-                                    , '<?php echo constant('GUDSTJENESTE_BTN_TXT'); ?>')"><?php echo constant('GUDSTJENESTE_BTN_TXT'); ?></a></li>
+            <div class="col-md-12">
+                    <div class="dropdown pull-left">
+                        <button id="btn_choose_category" type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown"><?php _e('Choose Group','mitwp'); ?>&nbsp;<span class='glyphicon glyphicon-folder-open'></span>&nbsp;&nbsp;<span class="caret"></span></button>
+                        <ul class="dropdown-menu">
+                            <li><a href="javascript:getiCalFromUrl('<?php echo constant('GUDSTJENESTE_ICAL_UID'); ?>'
+                            , '<?php echo constant('GUDSTJENESTE_BTN_TXT'); ?>')"><?php echo constant('GUDSTJENESTE_BTN_TXT'); ?></a></li>
 
-                                    <li><a href="javascript:getiCalFromUrl('<?php echo constant('JESHA_ICAL_UID'); ?>'
-                                    , '<?php echo constant('JESHA_BTN_TXT'); ?>')"><?php echo constant('JESHA_BTN_TXT'); ?></a></li>
+                            <li><a href="javascript:getiCalFromUrl('<?php echo constant('JESHA_ICAL_UID'); ?>'
+                            , '<?php echo constant('JESHA_BTN_TXT'); ?>')"><?php echo constant('JESHA_BTN_TXT'); ?></a></li>
 
-                                    <li><a href="javascript:getiCalFromUrl('<?php echo constant('JENTEKVELD_ICAL_UID'); ?>'
-                                    , '<?php echo constant('JENTEKVELD_BTN_TXT'); ?>')"><?php echo constant('JENTEKVELD_BTN_TXT'); ?></a></li>
+                            <li><a href="javascript:getiCalFromUrl('<?php echo constant('JENTEKVELD_ICAL_UID'); ?>'
+                            , '<?php echo constant('JENTEKVELD_BTN_TXT'); ?>')"><?php echo constant('JENTEKVELD_BTN_TXT'); ?></a></li>
 
-                                    <li><a href="javascript:getiCalFromUrl('<?php echo constant('ESC_ICAL_UID'); ?>'
-                                    , '<?php echo constant('ESC_BTN_TXT'); ?>')"><?php echo constant('ESC_BTN_TXT'); ?></a></li>
+                            <li><a href="javascript:getiCalFromUrl('<?php echo constant('ESC_ICAL_UID'); ?>'
+                            , '<?php echo constant('ESC_BTN_TXT'); ?>')"><?php echo constant('ESC_BTN_TXT'); ?></a></li>
 
-                                    <li><a href="javascript:getiCalFromUrl('<?php echo constant('KONFIRMANT_ICAL_UID'); ?>'
-                                    , '<?php echo constant('KONFIRMANT_BTN_TXT'); ?>')"><?php echo constant('KONFIRMANT_BTN_TXT'); ?></a></li>
+                            <li><a href="javascript:getiCalFromUrl('<?php echo constant('KONFIRMANT_ICAL_UID'); ?>'
+                            , '<?php echo constant('KONFIRMANT_BTN_TXT'); ?>')"><?php echo constant('KONFIRMANT_BTN_TXT'); ?></a></li>
 
-                                    <li><a href="javascript:getiCalFromUrl('<?php echo constant('TABAGO_ICAL_UID'); ?>'
-                                    , '<?php echo constant('TABAGO_BTN_TXT'); ?>')"><?php echo constant('TABAGO_BTN_TXT'); ?></a></li>
+                            <li><a href="javascript:getiCalFromUrl('<?php echo constant('TABAGO_ICAL_UID'); ?>'
+                            , '<?php echo constant('TABAGO_BTN_TXT'); ?>')"><?php echo constant('TABAGO_BTN_TXT'); ?></a></li>
 
-                                    <li><a href="javascript:getiCalFromUrl('<?php echo constant('BARNEKOR_ICAL_UID'); ?>'
-                                    , '<?php echo constant('BARNEKOR_BTN_TXT'); ?>')"><?php echo constant('BARNEKOR_BTN_TXT'); ?></a></li>
+                            <li><a href="javascript:getiCalFromUrl('<?php echo constant('BARNEKOR_ICAL_UID'); ?>'
+                            , '<?php echo constant('BARNEKOR_BTN_TXT'); ?>')"><?php echo constant('BARNEKOR_BTN_TXT'); ?></a></li>
 
-                                    <li><a href="javascript:getiCalFromUrl('<?php echo constant('ARRANGEMENT_ICAL_UID'); ?>'
-                                    , '<?php echo constant('ARRANGEMENT_BTN_TXT'); ?>')"><?php echo constant('ARRANGEMENT_BTN_TXT'); ?></a></li>
-                                </ul><!-- dropdown-menu -->
-                            </div><!-- dropdown -->
-            </div> <!-- COLUMN END -->
-            <div class="col-md-10">
-                <button type="button" id="btn_import" name="btn-start-import" class="btn btn-danger" onclick="saveImports()"><?php _e('Import','mitwp'); ?>&nbsp;<span class='glyphicon glyphicon-import'></span></button>
-            </div><!-- COLUMN END -->
+                            <li><a href="javascript:getiCalFromUrl('<?php echo constant('ARRANGEMENT_ICAL_UID'); ?>'
+                            , '<?php echo constant('ARRANGEMENT_BTN_TXT'); ?>')"><?php echo constant('ARRANGEMENT_BTN_TXT'); ?></a></li>
+                        </ul><!-- dropdown-menu -->
+                    </div><!-- dropdown -->
+                <button type="button" id="btn_import" name="btn-start-import" class="btn btn-danger pull-left" onclick="saveImports()"><?php _e('Import','mitwp'); ?>&nbsp;<span class='glyphicon glyphicon-import'></span></button>
+            </div> <!-- column -->
         </div><!-- ROW END -->
-        <p>&nbsp;</p>
+        <br>
         <!-- ROW -->
         <div class="row">
             <!--  COLUMN -->
             <div class="col-md-12">
                 <!-- Place holder for table -->
                 <div id="imp_table_placeholder">
-                    <table id="imp_table" class="table table-bordered table-hover table-responsive">
-                        <thead id="imp_table_head">
-                            <tr>
-                                <th class="text-center active"><?php _e('Import','mitwp'); ?></th>
-                                <th class="text-center active"><?php _e('Exist','mitwp'); ?></th>
-                                <th class="text-center active"><?php _e('Title','mitwp'); ?></th>
-                                <th class="text-center active"><?php _e('Begins','mitwp'); ?></th>
-                                <th class="text-center active"><?php _e('Ends','mitwp'); ?></th>
-                                <th class="text-center active"><?php _e('Description','mitwp'); ?></th>
-                            </tr>
-                        </thead>
-                        <tbody id="imp_table_body">
-                                <!-- dynamic rows here -->
-                        </tbody>
-                    </table>
-                </div>
-
+                    <div class="table-responsive">
+                        <table id="imp_table" class="table table-bordered table-hover">
+                            <thead id="imp_table_head">
+                                <tr>
+                                    <th class="text-center active"><?php _e('Import','mitwp'); ?></th>
+                                    <th class="text-center active"><?php _e('Exist','mitwp'); ?></th>
+                                    <th class="text-center active"><?php _e('Title','mitwp'); ?></th>
+                                    <th class="text-center active"><?php _e('Begins','mitwp'); ?></th>
+                                    <th class="text-center active"><?php _e('Ends','mitwp'); ?></th>
+                                    <th class="text-center active"><?php _e('Description','mitwp'); ?></th>
+                                </tr>
+                            </thead>
+                            <tbody id="imp_table_body">
+                                    <!-- dynamic rows here -->
+                            </tbody>
+                        </table>
+                    </div><!-- table responsive -->
+                </div> <!-- div table_placeholder -->
             </div> <!-- COLUMN END -->
         </div> <!-- ROW END END -->
     </div> <!-- container -->
